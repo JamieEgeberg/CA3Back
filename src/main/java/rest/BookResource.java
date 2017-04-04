6aa7ac4364ebf9c43fb2b2861d827c38c2599f82
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-04-04 10:11:19 +0200

    Added book resource yay

---
+++ src/main/java/rest/BookResource.java
@@ -0,0 +1,80 @@
+package rest;
+
+import com.google.gson.Gson;
+import entity.Book;
+import facades.BookFacade;
+import facades.IBookFacade;
+
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Created by Niki on 2017-04-04.
+ *
+ * @author Niki
+ */
+
+@Path("book")
+@RolesAllowed("User")
+public class BookResource {
+
+    private IBookFacade facade;
+    private EntityManagerFactory emf;
+    private static Gson gson = new Gson();
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of PersonResource
+     */
+    public BookResource() {
+        emf = Persistence.createEntityManagerFactory("PU");
+        facade = new BookFacade(emf);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAll() {
+        return gson.toJson(facade.getBooks());
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getById(@PathParam("id") int id) {
+        return gson.toJson(facade.getBook(id));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String add(String json) {
+        Book book = gson.fromJson(json, Book.class);
+        facade.addBook(book);
+        return gson.toJson(book);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String edit(String json) {
+        Book book = gson.fromJson(json, Book.class);
+        facade.editBook(book);
+        return gson.toJson(book);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String delete(@PathParam("id") int id) {
+        return gson.toJson(facade.deleteBook(id));
+        //return "{\"message\":\"Deleted book with id: \"" + id + "}";
+    }
+
+}
